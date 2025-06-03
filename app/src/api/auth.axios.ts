e8439f65b9c8ae80e35a00cc346644a915702d2f
Di3go0-0 <d.rincon@utp.edu.co>
2025-06-02 22:06:14 -0500

    update incus

--- app/src/api/auth.axios.ts
+++ app/src/api/auth.axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3010/auth";
+const API_BASE_URL = "http://10.221.217.20:3010/auth";
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
--- app/src/api/products.axios.ts
+++ app/src/api/products.axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3011/products";
+const API_BASE_URL = "http://10.221.217.123:3011/products";
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
